71d7471f4bc6a49f0b337c4e942553c569094112
seumsims <seumworldinbangladesh@gmail.com>
2023-01-23 11:56:49 +0600

    master php fundamentals constant and funtion on string completed

--- Ostad/constant/constant.php
+++ Ostad/constant/constant.php
@@ -1,13 +1,17 @@
 <?php
 define('TheConstant',5751);
+// wrong 
+//define('TheConstant',5758);
+// wrong
+// TheConstant = 123; 
 $constant = 'constant';
 echo "Value of TheConstant = {$constant('TheConstant')}";
 //$echoecho ='echo';
 echo "\n";
 // echo strlen($echoecho);
 
-$newstrlen = 'strlen';
+$stringlength = 'strlen';
 
-echo "Value of New Str Len = {$newstrlen('TheConstant')}";
-
-
+echo "Value of String Length = {$stringlength('TheConstant')}";
+echo "\n";
+echo "Value of String Length = {$stringlength($stringlength)}";
